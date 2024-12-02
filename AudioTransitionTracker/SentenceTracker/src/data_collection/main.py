137f78e0b4ae53ddedad33a936d066173581ce41
ChongXern <chongxern@gmail.com>
2024-12-02 13:50:42 -0500

    Remove redundant try except traceback lines

--- AudioTransitionTracker/SentenceTracker/src/data_collection/main.py
+++ AudioTransitionTracker/SentenceTracker/src/data_collection/main.py
@@ -10,36 +10,33 @@ if __name__ == "__main__":
             video_id = extract_id_from_url(video_id)
         print("Available modes:\nP: Parse\nS: Segment\nD: Divide\n")
         mode = input("Parse new URL or Segment existing AudioFile? ")
-        try:
-            if mode.lower() == 'p' or mode.lower() == 'parse':
-                existing_urls = load_urls("urls.txt")
-                #video_url = input("Input URL: ")
-                video_url = create_item_from_id(video_id, "u")
-                if video_url in existing_urls:
-                    print("URL already converted into AudioLecture object")
-                else:
-                    create_new_audio_lecture(video_url)
-                    existing_urls.add(video_url)
-                    save_url("../urls.txt", video_url)
-                    print("URL saved, converting to AudioLecture")
-            elif mode.lower() != 'q':
-                json_file = f"../../data/json_lectures/{video_id}.json"
-                audio_lec = parse_audio_lecture_from_json(json_file)
-                if audio_lec.is_full:
-                    if mode.lower() == 's' or mode.lower() == 'segment':
-                        segment_audio_lecture(audio_lec, 120000, 120000)
-                    elif mode.lower() == 'd' or mode.lower() == 'divide':
-                        #obtain audiolecture obj from json
-                        duration = audio_lec.duration
-                        print(f"FYI, total duration: {duration} ms or {duration // 60000} min {(duration % 60000) // 1000} s")
-                        unit_duration_ms = (int)(input("Unit duration length (seconds): ")) * 1000
-                        print(f"Recommended total segments for {unit_duration_ms} ms is {duration // unit_duration_ms}")
-                        total_count = (int)(input(f"Total segments: "))
-                        is_create_spectrogram = 'y' in input("Generate spectrogram? ")
-                        divide_audio_into_segments(audio_lec, unit_duration_ms, total_count, is_create_spectrogram)
-                else:
-                    print("Item is already segmented")
-        except FileNotFoundError: 
-            print("PRINTING TRACEBACK")
-            traceback.print_exc()
-        exit()
+        if mode.lower() == 'p' or mode.lower() == 'parse':
+            existing_urls = load_urls("urls.txt")
+            #video_url = input("Input URL: ")
+            video_url = create_item_from_id(video_id, "u")
+            if video_url in existing_urls:
+                print("URL already converted into AudioLecture object")
+            else:
+                create_new_audio_lecture(video_url)
+                existing_urls.add(video_url)
+                save_url("../urls.txt", video_url)
+                print("URL saved, converting to AudioLecture")
+        elif mode.lower() != 'q':
+            json_file = f"../../data/json_lectures/{video_id}.json"
+            audio_lec = parse_audio_lecture_from_json(json_file)
+            if audio_lec.is_full:
+                if mode.lower() == 's' or mode.lower() == 'segment':
+                    segment_audio_lecture(audio_lec, 120000, 120000)
+                elif mode.lower() == 'd' or mode.lower() == 'divide':
+                    #obtain audiolecture obj from json
+                    duration = audio_lec.duration
+                    print(f"FYI, total duration: {duration} ms or {duration // 60000} min {(duration % 60000) // 1000} s")
+                    unit_duration_ms = (int)(input("Unit duration length (seconds): ")) * 1000
+                    print(f"Recommended total segments for {unit_duration_ms} ms is {duration // unit_duration_ms}")
+                    total_count = (int)(input(f"Total segments: "))
+                    is_create_spectrogram = 'y' in input("Generate spectrogram? ")
+                    divide_audio_into_segments(audio_lec, unit_duration_ms, total_count, is_create_spectrogram)
+            else:
+                print("Item is already segmented")
+        else: 
+            exit()
